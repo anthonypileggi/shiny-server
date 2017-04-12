82d2964a12199e837123dc17b20f0b6249c800b2
Anthony Pileggi <anthony@main.localdomain>
2017-04-12 05:29:27 +0000

    Remove old typeform wrapper

--- typeform_data_explorer.R
+++
@@ -1,5 +0,0 @@
-# Install the latest version of the `typeformR` package
-#devtools::install_github('anthonypileggi/typeformR')
-
-# Run 'Typeform Data Explorer'
-typeformR::runShinyApp('data_explorer')
